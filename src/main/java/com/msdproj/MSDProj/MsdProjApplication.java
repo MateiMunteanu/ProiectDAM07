9c263bc5f263ad5cf409ad3f76602f1289dc6cf3
Matei <munteanu423@gmail.com>
2024-11-03 11:07:59 +0200

    Initial commit

---
+++ src/main/java/com/msdproj/MSDProj/MsdProjApplication.java
@@ -0,0 +1,13 @@
+package com.msdproj.MSDProj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsdProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsdProjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/msdproj/MSDProj/MsdProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msdproj.MSDProj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsdProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
